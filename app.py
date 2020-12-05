ae02fc20361b44956fea72e6ee08bfa81af3d65e
metiuu <48538697+metiuu@users.noreply.github.com>
2020-12-05 14:36:13 +1100

    finalized reading functions

--- app.py
+++ app.py
@@ -19,22 +19,27 @@ def upload_file():
 
     if request.method == "POST":
         if request.files:
-            spreadsheet = request.files["xls"]
+            book = request.files["xls"]
 
             # upload validation
-            if spreadsheet.filename == "":
+            if book.filename == "":
                 print("File must have a name")
                 return redirect(request.url)
 
-            if not check_file_type(spreadsheet.filename):
+            if not check_file_type(book.filename):
                 print("Illegal file type.")
                 return redirect(request.url)
             else:
-                filename = secure_filename(spreadsheet.filename)
+                filename = secure_filename(book.filename)
 
             print("File upload successful")
-            content = xlrd.open_workbook(file_contents=spreadsheet.read())
-            get_student_data(content)
+            content = xlrd.open_workbook(file_contents=book.read())
+            for sheet in content.sheets():
+                get_student_data(sheet)
+                total = count_worksheets(sheet)
+                print('Total worksheets done:', total)
+                total = 0
+                last_workbook(sheet)
             return redirect(request.url)
 
     return render_template("upload_file.html")
@@ -51,31 +56,27 @@ def check_file_type(filename):
         return
 
 
-def get_student_data(file):
+def get_student_data(sheet):
     # extract data from specific cells
-    worksheet = file.sheet_by_index(0) # maybe we can make it so that the app goes through all sheets in a xls/xlsx file?
-    print('Student name:',worksheet.cell(1,7).value)
-    print('Subject:',worksheet.cell(2,1).value)
-    print('Month:',worksheet.cell(2,0).value, int(worksheet.cell(2,3).value))
+    print('Student name:',sheet.cell(1,7).value)
+    print('Subject:',sheet.cell(2,1).value)
+    print('Month:',sheet.cell(2,0).value, int(sheet.cell(2,3).value))
     print()
 
-def count_worksheets(file):
-    # total worksheets done (starting row 6, columns 5-14)
-    worksheet = file.sheet_by_index(0) # maybe we can make it so that the app goes through all sheets in a xls/xlsx file?
+def count_worksheets(sheet):
     total = 0
-    for row in range(5,worksheet.nrows):
+    for row in range(5,sheet.nrows):
         for col in range(4,14):
-            cell_type = worksheet.cell_type(row,col)
+            cell_type = sheet.cell_type(row,col)
 
             if not cell_type == xlrd.XL_CELL_EMPTY:
                 total += 1
     return total
 
-def last_workbook(file):
+def last_workbook(sheet):
     # last workbook done (last row of spreadsheet, don't know how many rows there are)
     # last row(starting row 6), columns 2 and 3
-    worksheet = file.sheet_by_index(0) # maybe we can make it so that the app goes through all sheets in a xls/xlsx file?
-    return worksheet.col_values(1,5)[-1] + str(int(worksheet.col_values(2,5)[-1]))
+    print('Last worksheet done:', sheet.col_values(1,5)[-1] + ' ' + str(int(sheet.col_values(2,5)[-1])))
     
 
 
